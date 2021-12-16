41acceeb9acf9ef81364d8173a8104a60d5dbea5
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-16 07:44:13 +0200

    adjusted image size.

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import './card-list.styles.css';
 import CardComponent from "../card/card.component";
+
 const cardListComponent = ({monsters}) => (
     <div className={"card-list"}>
         {monsters.map((monster) => (
-            <h1 >
+            <h1 key={monster.id}>
 
-                <CardComponent key={monster.id} monster={monster}/>
+                <CardComponent monster={monster}/>
             </h1>))}
 
     </div>)
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import './card.styles.css'
 const cardComponent = ({monster})=>(
     <div className={"card-container"}>
-        <img src={`https://robohash.org/${monster.id}?set=set2&size=80x80`}/>
+        <img src={`https://robohash.org/${monster.id}?set=set2&size=80x80`} alt={"monster-images"}/>
         {monster.name}
         <h1>{monster.email}</h1>
     </div>
