3bf75b3328565d3af1d90dfcdee5ca2d1475bb57
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-16 05:47:46 +0200

    setting state with some placeholder data.

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import './App.css';
 import {Component} from "react";
 
 class App extends Component {
-    state = [
-        {name: 'Frankenstein'},
-        {name: 'Dracula'}
-        , {name: 'Zombie'},
-    ]
-    render = () => (
-        <div className="App">
-            {
-                this.state.map((element, i) => <h1 key={i}> {element.name}</h1>)
-            }
-        </div>
-    );
+    state = {
+        monsters: []
+    }
+
+
+    componentDidMount() {
+        const endpoint = 'https://jsonplaceholder.typicode.com/users'
+        fetch(endpoint)
+            .then(result => result.json())
+            .then(result => {
+                this.setState({monsters: result})
+            })
+    }
+
+
+    render = () => (<div className="App">
+            {this.state.monsters.map((element) => <h1 key={element.id}> {element.name}</h1>)}
+        </div>);
 }
 
 export default App;
