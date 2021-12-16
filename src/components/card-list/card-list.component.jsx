43b9b7e97522afad235259c452eb1132af5c8024
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-16 07:24:31 +0200

    created the card component.

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import './card-list.styles.css';
-
+import CardComponent from "../card/card.component";
 const cardListComponent = ({monsters}) => (
     <div className={"card-list"}>
-        {monsters.map((element) => <h1 key={element.id}> {element.name}</h1>)}
+        {monsters.map((element) => (
+            <h1 key={element.id}>
+                <CardComponent monster={element}/>
+            </h1>))}
 
     </div>)
 
