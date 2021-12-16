64b8612a4da7eb66c87ca2f7837c20aea0afd252
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-16 07:46:33 +0200

    adjusted image size.

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import './card.styles.css'
 const cardComponent = ({monster})=>(
     <div className={"card-container"}>
-        <img src={`https://robohash.org/${monster.id}?set=set2&size=80x80`} alt={"monster-images"}/>
-        {monster.name}
-        <h1>{monster.email}</h1>
+        <img src={`https://robohash.org/${monster.id}?set=set2&size=180x180`} alt={"monster-images"}/>
+        <h2>{monster.name}</h2>
+        <p>{monster.email}</p>
     </div>
 )
 export default cardComponent;
